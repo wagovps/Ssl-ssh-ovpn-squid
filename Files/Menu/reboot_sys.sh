6106483d1e2ee0ffc5f2d8c5dec0470807428019
wagovps <49498862+wagovps@users.noreply.github.com>
2020-05-29 11:35:25 -0700

    Update reboot_sys.sh

--- Files/Menu/reboot_sys.sh
+++ Files/Menu/reboot_sys.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-# Script by : Hiratechi
+# Script by : WaGo
 clear
 echo -e "\e[0m                                                   "
 echo -e "\e[94m[][][]======================================[][][]"
 echo -e "\e[0m                                                   "
-echo -e "\e[93m           AutoScriptVPS by  Hiratechi           "
+echo -e "\e[93m           AutoScriptVPS by  WaGo           "
 echo -e "\e[0m                                                   "
 echo -e "\e[93m            [1] Reboot Now"
 echo -e "\e[93m            [2] Reboot Time"
