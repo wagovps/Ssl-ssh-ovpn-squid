53402291ddde056cc9cc6e3ca47933e86c7e8639
wagovps <49498862+wagovps@users.noreply.github.com>
2020-05-29 11:22:04 -0700

    Update clearcache.sh

--- Files/Menu/clearcache.sh
+++ Files/Menu/clearcache.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-# Script by : Hiratechi
+# Script by : WaGo
 echo "echo 3 > /proc/sys/vm/drop_caches"
 clear
 echo -e "\e[0m                                                   "
 echo -e "\e[94m[][][]======================================[][][]"
 echo -e "\e[0m                                                   "
-echo -e "\e[93m           AutoScriptVPS by  Hiratechi           "
+echo -e "\e[93m           AutoScriptVPS by  WaGo           "
 echo -e "\e[0m                                                   "
 echo -e "\e[93m                  Cache  Cleared                  "
 echo -e "\e[0m                                                   "
