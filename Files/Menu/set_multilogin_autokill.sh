b7ff5afac8bc8aec2e3b783b3bd6923550fcbfea
wagovps <49498862+wagovps@users.noreply.github.com>
2020-05-29 11:39:25 -0700

    Update set_multilogin_autokill.sh

--- Files/Menu/set_multilogin_autokill.sh
+++ Files/Menu/set_multilogin_autokill.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-# Script by : Hiratechi
+# Script by : WaGo
 clear
 echo -e "\e[0m                                                   "
 echo -e "\e[94m[][][]======================================[][][]"
 echo -e "\e[0m                                                   "
-echo -e "\e[93m           AutoScriptVPS by  Hiratechi           "
+echo -e "\e[93m           AutoScriptVPS by  WaGo           "
 echo -e "\e[0m                                                   "
 echo -e "\e[93m            [1]  AutoKill After 5 Minutes"
 echo -e "\e[93m            [2]  AutoKill After 15 Minutes"
@@ -25,7 +25,7 @@ case $AutoKill in
 		echo -e "\e[0m                                                   "
 		echo -e "\e[94m[][][]======================================[][][]"
 		echo -e "\e[0m                                                   "
-		echo -e "\e[93m           AutoScriptVPS by  Hiratechi           "
+		echo -e "\e[93m           AutoScriptVPS by  WaGo           "
 		echo -e "\e[0m                                                   "
 		echo -e "\e[93m             Allowed MultiLogin : "$MultiLogin
 		echo -e "\e[93m             AutoKill Every     : 5 Minutes"
@@ -43,7 +43,7 @@ case $AutoKill in
 		echo -e "\e[0m                                                   "
 		echo -e "\e[94m[][][]======================================[][][]"
 		echo -e "\e[0m                                                   "
-		echo -e "\e[93m           AutoScriptVPS by  Hiratechi           "
+		echo -e "\e[93m           AutoScriptVPS by  WaGo           "
 		echo -e "\e[0m                                                   "
 		echo -e "\e[93m             Allowed MultiLogin : "$MultiLogin
 		echo -e "\e[93m             AutoKill Every     : 15 Minutes"
@@ -61,7 +61,7 @@ case $AutoKill in
 		echo -e "\e[0m                                                   "
 		echo -e "\e[94m[][][]======================================[][][]"
 		echo -e "\e[0m                                                   "
-		echo -e "\e[93m           AutoScriptVPS by  Hiratechi           "
+		echo -e "\e[93m           AutoScriptVPS by  WaGo           "
 		echo -e "\e[0m                                                   "
 		echo -e "\e[93m             Allowed MultiLogin : "$MultiLogin
 		echo -e "\e[93m             AutoKill Every     : 30 Minutes"
@@ -75,7 +75,7 @@ case $AutoKill in
 		echo -e "\e[0m                                                   "
 		echo -e "\e[94m[][][]======================================[][][]"
 		echo -e "\e[0m                                                   "
-		echo -e "\e[93m           AutoScriptVPS by  Hiratechi           "
+		echo -e "\e[93m           AutoScriptVPS by  WaGo           "
 		echo -e "\e[0m                                                   "
 		echo -e "\e[93m              AutoKill MultiLogin Turned               "
 		echo -e "\e[0m                                                   "
