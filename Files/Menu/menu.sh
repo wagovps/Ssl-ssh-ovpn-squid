70ff28bd007b2ba0f8214695a9e224d4f30a0967
wagovps <49498862+wagovps@users.noreply.github.com>
2020-05-29 11:34:12 -0700

    Update menu.sh

--- Files/Menu/menu.sh
+++ Files/Menu/menu.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# Script by : Hiratechi
+# Script by : WaGo
 clear
 echo -e "\e[0m                                                   "
-echo -e "\e[93m           AutoScriptVPS by  Hiratechi           "
-echo -e "\e[93m                  a.k.a reputation                "
+echo -e "\e[93m           AutoScriptVPS by  Warren           "
+echo -e "\e[93m                  a.k.a WaGo                "
 echo -e "\e[0m                                                   "
 echo -e "\e[93m        COMMANDS [menu/accounts/options/server]            "
 echo -e "\e[0m                                                   "
