014729a3e1d044d3dad0e0c35ff976f074d2909f
wagovps <49498862+wagovps@users.noreply.github.com>
2020-05-29 11:29:51 -0700

    Update edit_openssh.sh

--- Files/Menu/edit_openssh.sh
+++ Files/Menu/edit_openssh.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Script by : Hiratechi
+# Script by : WaGo
 rm -f /root/opensshport
 rm -f /root/dropbearport
 rm -f /root/stunnel4port
@@ -36,7 +36,7 @@ clear
 echo -e "\e[0m                                                   "
 echo -e "\e[94m[][][]======================================[][][]"
 echo -e "\e[0m                                                   "
-echo -e "\e[93m           AutoScriptVPS by  Hiratechi           "
+echo -e "\e[93m           AutoScriptVPS by  WaGo           "
 echo -e "\e[0m                                                   "
 echo -e "\e[93m                  OpenSSH  Ports                  "
 echo -e "\e[93m                   "$opensshport
@@ -86,7 +86,7 @@ if [ $? -eq 0 ]; then
 	echo -e "\e[0m                                                   "
 	echo -e "\e[94m[][][]======================================[][][]"
 	echo -e "\e[0m                                                   "
-	echo -e "\e[93m           AutoScriptVPS by  Hiratechi           "
+	echo -e "\e[93m           AutoScriptVPS by  WaGo           "
 	echo -e "\e[0m                                                   "
 	echo -e "\e[93m                  OpenSSH  Ports                  "
 	echo -e "\e[93m                   "$opensshport
